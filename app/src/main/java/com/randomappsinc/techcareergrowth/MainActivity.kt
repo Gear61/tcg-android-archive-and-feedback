34c5b2ee2fb2b3ef4adcee2229f3c547b3e02dec
Gear61 <chioualexander@yahoo.com>
2022-01-15 16:57:43 -0800

    Fix theme colors

--- app/src/main/java/com/randomappsinc/techcareergrowth/MainActivity.kt
+++ app/src/main/java/com/randomappsinc/techcareergrowth/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.randomappsinc.techcareergrowth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,7 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        setTitle(R.string.full_app_name)
+    override fun startActivityForResult(intent: Intent?, requestCode: Int) {
+        super.startActivityForResult(intent, requestCode)
+        overridePendingTransition(R.anim.slide_left_out, R.anim.slide_left_in)
     }
 }
