e67c7689a627195b26f27d4a98f2540460322f68
Gear61 <chioualexander@yahoo.com>
2022-01-15 16:51:02 -0800

    Add splash screen

--- app/src/main/java/com/randomappsinc/techcareergrowth/MainActivity.kt
+++ app/src/main/java/com/randomappsinc/techcareergrowth/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTitle(R.string.full_app_name)
     }
 }
---
+++ app/src/main/java/com/randomappsinc/techcareergrowth/init/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.randomappsinc.techcareergrowth.init
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.randomappsinc.techcareergrowth.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Kill activity if it's above an existing stack due to launcher bug
+        if (!isTaskRoot
+            && intent.hasCategory(Intent.CATEGORY_LAUNCHER)
+            && intent.action != null && intent.action == Intent.ACTION_MAIN) {
+            finish()
+            return
+        }
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
