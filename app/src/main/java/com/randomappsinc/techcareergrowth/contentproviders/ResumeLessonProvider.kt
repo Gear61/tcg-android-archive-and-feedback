c2908daf0b6c78f358dc95beb90551f4689ed39e
Gear61 <chioualexander@yahoo.com>
2022-01-16 23:02:53 -0800

    Add beginning of resume lesson 1

--- app/src/main/java/com/randomappsinc/techcareergrowth/contentproviders/ResumeLessonProvider.kt
+++ app/src/main/java/com/randomappsinc/techcareergrowth/contentproviders/ResumeLessonProvider.kt
@@ -1,7 +1,10 @@
 package com.randomappsinc.techcareergrowth.contentproviders
 
 import android.content.Context
+import com.randomappsinc.techcareergrowth.R
 import com.randomappsinc.techcareergrowth.models.Lesson
+import com.randomappsinc.techcareergrowth.models.Question
+import com.randomappsinc.techcareergrowth.persistence.PreferencesManager
 
 object ResumeLessonProvider {
 
@@ -10,7 +13,35 @@ object ResumeLessonProvider {
     fun getLessons(context: Context): List<Lesson> {
         if (lessons == null) {
             lessons = mutableListOf()
+            lessons!!.add(getLesson1(context = context))
         }
         return lessons!!
+    }
+
+    private fun getLesson1(context: Context): Lesson {
+        val questions: MutableList<Question> = mutableListOf()
+        questions.add(
+            Question(
+                textResId = R.string.resume_1_question_1,
+                optionsTextIds = R.array.true_false_options,
+                correctAnswerId = R.string.false_option
+            )
+        )
+        questions.add(
+            Question(
+                textResId = R.string.resume_1_question_2,
+                optionsTextIds = R.array.resume_1_question_2_options,
+                correctAnswerId = R.string.resume_1_question_2_option_1
+            )
+        )
+
+        val lessonId = "resume_1"
+        return Lesson(
+            id = lessonId,
+            nameResId = R.string.resume_lesson_1_name,
+            youtubeVideoId = "MByD2CTwfmM",
+            questions = questions,
+            isCompleted = PreferencesManager(context).getLessonCompletionStatus(lessonId)
+        )
     }
 }
