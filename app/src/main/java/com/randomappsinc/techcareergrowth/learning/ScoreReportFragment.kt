7d52f25306bf12e17bafdf4b4ad81bc7c9ff5e80
Gear61 <chioualexander@yahoo.com>
2022-01-27 16:54:24 -0800

    Add confetti when user gets a perfect score

--- app/src/main/java/com/randomappsinc/techcareergrowth/learning/ScoreReportFragment.kt
+++ app/src/main/java/com/randomappsinc/techcareergrowth/learning/ScoreReportFragment.kt
@@ -6,6 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.randomappsinc.techcareergrowth.databinding.ScoreReportBinding
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.Position
+import nl.dionsegijn.konfetti.core.Rotation
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import java.util.concurrent.TimeUnit
 
 class ScoreReportFragment: Fragment() {
 
@@ -30,10 +35,7 @@ class ScoreReportFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bindView()
-    }
 
-    fun bindView() {
         val activity = requireActivity() as LessonActivity
         binding.scoreMessage.text = activity.viewState.scoreMessage
         binding.scoreText.text = activity.viewState.scoreText
@@ -53,6 +55,20 @@ class ScoreReportFragment: Fragment() {
         if (activity.viewState.gotPerfectScore) {
             binding.retakeQuizButton.visibility = View.GONE
             binding.relearnContentButton.visibility = View.GONE
+
+            val party = Party(
+                angle = 270,
+                speed = 0f,
+                maxSpeed = 175f,
+                damping = 0.9f,
+                spread = 90,
+                colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+                emitter = Emitter(duration = 5000, TimeUnit.MILLISECONDS).max(1500),
+                position = Position.Relative(0.5, 1.0),
+                rotation = Rotation.enabled()
+            )
+            binding.konfettiView.visibility = View.VISIBLE
+            binding.konfettiView.start(party)
         } else {
             binding.retakeQuizButton.visibility = View.VISIBLE
             binding.relearnContentButton.visibility = View.VISIBLE
