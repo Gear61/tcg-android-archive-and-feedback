368e135333ac29fbee92ad0474c49c0c1942d36e
Gear61 <chioualexander@yahoo.com>
2022-01-30 16:38:51 -0800

    Add resources for productivity lesson 2

--- app/src/main/java/com/randomappsinc/techcareergrowth/contentproviders/ProductivityLessonProvider.kt
+++ app/src/main/java/com/randomappsinc/techcareergrowth/contentproviders/ProductivityLessonProvider.kt
@@ -15,6 +15,7 @@ object ProductivityLessonProvider {
         if (lessons == null) {
             lessons = mutableListOf()
             lessons!!.add(getLesson1(context = context))
+            lessons!!.add(getLesson2(context = context))
         }
         return lessons!!
     }
@@ -63,6 +64,55 @@ object ProductivityLessonProvider {
             type = LessonType.PRODUCTIVITY,
             nameResId = R.string.productivity_lesson_1_name,
             youtubeVideoId = "XqBA2fkvUTg",
+            questions = questions,
+            isCompleted = PreferencesManager(context).getLessonCompletionStatus(lessonId)
+        )
+    }
+
+    private fun getLesson2(context: Context): Lesson {
+        val questions: MutableList<Question> = mutableListOf()
+        questions.add(
+            Question(
+                textResId = R.string.productivity_1_question_1,
+                optionsListResId = R.array.productivity_1_question_1_options,
+                correctAnswerId = R.string.productivity_1_question_1_option_2
+            )
+        )
+        questions.add(
+            Question(
+                textResId = R.string.productivity_1_question_2,
+                optionsListResId = R.array.true_false_options,
+                correctAnswerId = R.string.false_option
+            )
+        )
+        questions.add(
+            Question(
+                textResId = R.string.productivity_1_question_3,
+                optionsListResId = R.array.productivity_1_question_3_options,
+                correctAnswerId = R.string.productivity_1_question_3_option_3
+            )
+        )
+        questions.add(
+            Question(
+                textResId = R.string.productivity_1_question_4,
+                optionsListResId = R.array.true_false_options,
+                correctAnswerId = R.string.true_option
+            )
+        )
+        questions.add(
+            Question(
+                textResId = R.string.productivity_1_question_5,
+                optionsListResId = R.array.productivity_1_question_5_options,
+                correctAnswerId = R.string.productivity_1_question_5_option_4
+            )
+        )
+
+        val lessonId = "productivity_2"
+        return Lesson(
+            id = lessonId,
+            type = LessonType.PRODUCTIVITY,
+            nameResId = R.string.productivity_lesson_1_name,
+            youtubeVideoId = "3gvjpMhhfnY",
             questions = questions,
             isCompleted = PreferencesManager(context).getLessonCompletionStatus(lessonId)
         )
