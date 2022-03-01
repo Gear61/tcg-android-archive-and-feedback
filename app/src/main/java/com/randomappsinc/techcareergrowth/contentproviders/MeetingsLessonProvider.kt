193983159b91b99721127270d6fb6b65f1b15482
Gear61 <chioualexander@yahoo.com>
2022-03-01 14:54:43 -0800

    Add meeting goals lesson

--- app/src/main/java/com/randomappsinc/techcareergrowth/contentproviders/MeetingsLessonProvider.kt
+++ app/src/main/java/com/randomappsinc/techcareergrowth/contentproviders/MeetingsLessonProvider.kt
@@ -15,6 +15,7 @@ object MeetingsLessonProvider {
         if (lessons == null) {
             lessons = mutableListOf()
             lessons!!.add(getLesson1(context = context))
+            lessons!!.add(getLesson2(context = context))
         }
         return lessons!!
     }
@@ -23,37 +24,37 @@ object MeetingsLessonProvider {
         val questions: MutableList<Question> = mutableListOf()
         questions.add(
             Question(
-                textResId = R.string.meetings_question_1,
-                optionsListResId = R.array.meetings_question_1_options,
-                correctAnswerId = R.string.meetings_question_1_option_2
+                textResId = R.string.meetings_1_question_1,
+                optionsListResId = R.array.meetings_1_question_1_options,
+                correctAnswerId = R.string.meetings_1_question_1_option_2
             )
         )
         questions.add(
             Question(
-                textResId = R.string.meetings_question_2,
+                textResId = R.string.meetings_1_question_2,
                 optionsListResId = R.array.true_false_options,
                 correctAnswerId = R.string.false_option
             )
         )
         questions.add(
             Question(
-                textResId = R.string.meetings_question_3,
-                optionsListResId = R.array.meetings_question_3_options,
-                correctAnswerId = R.string.meetings_question_3_option_1
+                textResId = R.string.meetings_1_question_3,
+                optionsListResId = R.array.meetings_1_question_3_options,
+                correctAnswerId = R.string.meetings_1_question_3_option_1
             )
         )
         questions.add(
             Question(
-                textResId = R.string.meetings_question_4,
+                textResId = R.string.meetings_1_question_4,
                 optionsListResId = R.array.true_false_options,
                 correctAnswerId = R.string.true_option
             )
         )
         questions.add(
             Question(
-                textResId = R.string.meetings_question_5,
+                textResId = R.string.meetings_1_question_5,
                 optionsListResId = R.array.meetings_question_5_options,
-                correctAnswerId = R.string.meetings_question_5_option_2
+                correctAnswerId = R.string.meetings_1_question_5_option_2
             )
         )
 
@@ -63,6 +64,33 @@ object MeetingsLessonProvider {
             type = LessonType.MEETINGS,
             nameResId = R.string.meetings_lesson_1_name,
             youtubeVideoId = "e6Ej_9mEc10",
+            questions = questions,
+            isCompleted = PreferencesManager(context).getLessonCompletionStatus(lessonId)
+        )
+    }
+    private fun getLesson2(context: Context): Lesson {
+        val questions: MutableList<Question> = mutableListOf()
+        questions.add(
+            Question(
+                textResId = R.string.meetings_2_question_1,
+                optionsListResId = R.array.meetings_2_question_1_options,
+                correctAnswerId = R.string.meetings_2_question_1_option_3
+            )
+        )
+        questions.add(
+            Question(
+                textResId = R.string.meetings_2_question_2,
+                optionsListResId = R.array.meetings_2_question_2_options,
+                correctAnswerId = R.string.meetings_2_question_2_option_2
+            )
+        )
+
+        val lessonId = "meetings_2"
+        return Lesson(
+            id = lessonId,
+            type = LessonType.MEETINGS,
+            nameResId = R.string.meetings_lesson_2_name,
+            youtubeVideoId = "ZzQquAy7gwQ",
             questions = questions,
             isCompleted = PreferencesManager(context).getLessonCompletionStatus(lessonId)
         )
