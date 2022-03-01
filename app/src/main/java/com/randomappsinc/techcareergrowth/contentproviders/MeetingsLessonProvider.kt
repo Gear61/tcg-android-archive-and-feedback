a7e54089719108e89f13b65c2dfb90eb39a907e4
Gear61 <chioualexander@yahoo.com>
2022-03-01 14:44:44 -0800

    Add meeting framework lesson

--- app/src/main/java/com/randomappsinc/techcareergrowth/contentproviders/MeetingsLessonProvider.kt
+++ app/src/main/java/com/randomappsinc/techcareergrowth/contentproviders/MeetingsLessonProvider.kt
@@ -1,7 +1,11 @@
 package com.randomappsinc.techcareergrowth.contentproviders
 
 import android.content.Context
+import com.randomappsinc.techcareergrowth.R
 import com.randomappsinc.techcareergrowth.models.Lesson
+import com.randomappsinc.techcareergrowth.models.LessonType
+import com.randomappsinc.techcareergrowth.models.Question
+import com.randomappsinc.techcareergrowth.persistence.PreferencesManager
 
 object MeetingsLessonProvider {
 
@@ -10,7 +14,57 @@ object MeetingsLessonProvider {
     fun getLessons(context: Context): List<Lesson> {
         if (lessons == null) {
             lessons = mutableListOf()
+            lessons!!.add(getLesson1(context = context))
         }
         return lessons!!
+    }
+
+    private fun getLesson1(context: Context): Lesson {
+        val questions: MutableList<Question> = mutableListOf()
+        questions.add(
+            Question(
+                textResId = R.string.meetings_question_1,
+                optionsListResId = R.array.meetings_question_1_options,
+                correctAnswerId = R.string.meetings_question_1_option_2
+            )
+        )
+        questions.add(
+            Question(
+                textResId = R.string.meetings_question_2,
+                optionsListResId = R.array.true_false_options,
+                correctAnswerId = R.string.false_option
+            )
+        )
+        questions.add(
+            Question(
+                textResId = R.string.meetings_question_3,
+                optionsListResId = R.array.meetings_question_3_options,
+                correctAnswerId = R.string.meetings_question_3_option_1
+            )
+        )
+        questions.add(
+            Question(
+                textResId = R.string.meetings_question_4,
+                optionsListResId = R.array.true_false_options,
+                correctAnswerId = R.string.true_option
+            )
+        )
+        questions.add(
+            Question(
+                textResId = R.string.meetings_question_5,
+                optionsListResId = R.array.meetings_question_5_options,
+                correctAnswerId = R.string.meetings_question_5_option_2
+            )
+        )
+
+        val lessonId = "meetings_1"
+        return Lesson(
+            id = lessonId,
+            type = LessonType.MEETINGS,
+            nameResId = R.string.meetings_lesson_1_name,
+            youtubeVideoId = "e6Ej_9mEc10",
+            questions = questions,
+            isCompleted = PreferencesManager(context).getLessonCompletionStatus(lessonId)
+        )
     }
 }
