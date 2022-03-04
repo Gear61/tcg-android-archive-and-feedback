7f4d9f920b59155820f17024cc511b0b60157278
Gear61 <chioualexander@yahoo.com>
2022-03-04 15:52:39 -0800

    Polish lesson list

--- app/src/main/java/com/randomappsinc/techcareergrowth/lessonlist/LessonListActivity.kt
+++ app/src/main/java/com/randomappsinc/techcareergrowth/lessonlist/LessonListActivity.kt
@@ -1,22 +1,15 @@
 package com.randomappsinc.techcareergrowth.lessonlist
 
-import android.content.DialogInterface
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.randomappsinc.techcareergrowth.R
 import com.randomappsinc.techcareergrowth.common.Constants
 import com.randomappsinc.techcareergrowth.contentproviders.LessonProvider
-import com.randomappsinc.techcareergrowth.databinding.ActivityMainBinding
 import com.randomappsinc.techcareergrowth.databinding.LessonListBinding
-import com.randomappsinc.techcareergrowth.home.HomepageAdapter
 import com.randomappsinc.techcareergrowth.learning.LessonActivity
 import com.randomappsinc.techcareergrowth.models.Lesson
 import com.randomappsinc.techcareergrowth.models.LessonType
-import com.randomappsinc.techcareergrowth.persistence.PreferencesManager
-import com.randomappsinc.techcareergrowth.util.UIUtil
 
 class LessonListActivity: AppCompatActivity(), LessonListAdapter.Listener {
 
@@ -24,8 +17,11 @@ class LessonListActivity: AppCompatActivity(), LessonListAdapter.Listener {
         super.onCreate(savedInstanceState)
         val binding = LessonListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         val lessonType = intent.getSerializableExtra(Constants.LESSON_TYPE_KEY) as LessonType
+        setTitle(lessonType.overallLabelId)
+
         val adapter = LessonListAdapter(
             lessons = LessonProvider.getLessonList(
                 type = lessonType,
